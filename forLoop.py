f1297b4da372f4e9b4ac005167bc62ec46111483
jinarma <jinarma@gmail.com>
2020-09-06 22:05:43 +0530

    first commit

---
+++ forLoop.py
@@ -0,0 +1,7 @@
+print('my name is')
+for i in range(10):
+	if i%2==0:
+		continue
+	elif i>7:
+		break
+	print("Jinu ("+str(i)+")")
---
+++ whileEquivalentToForLoop.py
@@ -0,0 +1,9 @@
+print('my name is')
+i=0
+while i<10:
+	i=i+1
+	if i%2==0:
+		continue
+	elif i>7:
+		break
+	print("Jinu ("+str(i)+")")
