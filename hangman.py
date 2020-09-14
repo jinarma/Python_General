8fecaa3568ec64d4a07174c6948f9f5780c3609b
jinarma <jinarma@gmail.com>
2020-09-14 16:14:28 +0530

    added game function

--- hangman.py
+++ hangman.py
@@ -1,9 +1,23 @@
 import os
 
-def gameLogic():
+def gameLogic(name):
+	pseudoString=''
+	test=0
+	length=len(name)
 	oneCharacter=input()
-
+	numberOfChoices=3
+	for i in range(0, length):
+		if oneCharacter==name[i]:
+			print(oneCharacter, end='')
+			pseudoString=pseudoString[ : i]+oneCharacter
+		else:
+			print("*", end='')
+			test=test+1
+		if test==length:
+			print('Wrong guess!\nYou have '+str(numberOfChoices)+' choices left.')
+			numberOfChoices=numberOfChoices-1
+	print('\n'+pseudoString)
 movieName=input("Enter the name of a movie: ")
 os.system('cls')
 print("Guess the letters of the movie!")
-gameLogic()
+gameLogic(movieName)
