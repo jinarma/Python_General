7abac1f9e07d08e37fa6932a29b99073304c88d8
Jinarma <jinarma@gmail.com>
2022-02-14 13:07:43 +0530

    using fStrings now

--- dictionary.py
+++ dictionary.py
@@ -1,7 +1,8 @@
 def car_listing(car_prices):
   result = ""
-  for car_name, car_price in car_prices.items():
-    result += "{} costs {} dollars".format(car_name, car_price) + "\n"
+  result = "".join(f"{list(car_prices.values())} costs {list(car_prices.keys())} dollars\n")
+  """ for car_name, car_price in car_prices.items():
+    result += f"{car_name} costs {car_price} dollars\n" """
   return result
 
 print(car_listing({"Kia Soul":19000, "Lamborghini Diablo":55000, "Ford Fiesta":13000, "Toyota Prius":24000}))
