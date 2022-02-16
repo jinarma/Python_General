135fe84bf9796e55ce8c8cc7bd2c37c04380bac4
jinarma <jinarma@gmail.com>
2022-02-16 11:26:45 +0530

    nothing interesting

---
+++ absAndRound.py
@@ -0,0 +1,11 @@
+print(abs(20))
+print(abs(-30))
+print(abs(-30.4))
+print(abs(-30.5))
+print()
+print(round(30.4))
+print(round(30.5))
+print(round(-30.5))
+print(round(-30.4))
+
+# abs != round != ceil != floor
--- bitwiseOp.py
+++ bitwiseOp.py
@@ -1,15 +1,18 @@
-a = 6	# 1001
-b = 9	# 0110
-c = [10]
-print(bin(a))
-print(bin(b))
+a = 5	# 0101
+b = 9	# 1001
+c = []
+print(a, bin(a))
+print(b, bin(b))
+print(a+b, bin(a+b))
 print()
-c.append(b & a)	# AND
-c.append(b | a)	# OR
+# c.append(b & a)	# AND
+# c.append(b | a)	# OR
 c.append(~b)	# NOT
-c.append(~(b & a))	# NAND
-c.append(~(b | a))	# NOR
-c.append(b ^ a)	# XOR
+# c.append(~(b & a))	# NAND
+# c.append(~(b | a))	# NOR
+# c.append(b ^ a)	# XOR
+# c.append(~(a ^ b)) # XNOR
+# c.append((~(a ^ b))+1) # 2s compliment
 for i in c:
-	print(i, end=)
-	print(bin(i))
+	print(i, bin(i), sep=' ')
+print(c)
