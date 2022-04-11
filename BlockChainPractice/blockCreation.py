93611d36023dcd74fe900e3df65e51e9af44915b
Jinarma <jinarma@gmail.com>
2022-04-11 09:02:14 +0530

    removed property decorator

--- BlockChainPractice/blockCreation.py
+++ BlockChainPractice/blockCreation.py
@@ -1,11 +1,13 @@
 from hashlib import sha256, sha1
-from time import time
+from time import time, gmtime, localtime
+
+from numpy import block
 
 class Block:
-	def __init__(self, prev_hash:str, txn:str, timestamp=time()) -> None:
+	def __init__(self, prev_hash:str, txn:str, timestamp=gmtime()) -> None:
 		self.prev_hash = prev_hash
 		self.txn = txn
-		self.timestamp = timestamp
+		self.timestamp = '-'.join(list(map(str, [timestamp.tm_year, timestamp.tm_mon, timestamp.tm_mday])))+ ' ' + ':'.join(list(map(str, [timestamp.tm_hour, timestamp.tm_min, timestamp.tm_sec])))
 		self.block_data = str(txn) + '-' + prev_hash
 		self.block_hash = sha1(self.block_data.encode()).hexdigest()
 
@@ -15,23 +17,22 @@ class Chain:
 		self.generate_genesis()
 	
 	def generate_genesis(self):
-		self.chain.append(Block('aadhar', 0))
+		self.chain.append(Block(input(), 0))
 
 	def next_block(self, txn):
-		prev_hash = self.last_block.block_hash
+		prev_hash = self.last_block().block_hash
 		self.txn = txn
 		self.chain.append(Block(prev_hash, self.txn))
 	
-	@property
-	def last_block(self) -> Block:
-		return self.chain[-1]
+	def last_block(self):
+		return self.chain[-1]	
 
 	def size_of_chain(self) -> int:
 		return len(self.chain)
 
 	def display(self):
 		for ele in self.chain:
-			print(ele.block_data, ele.block_hash)
+			print(ele.block_data, ele.block_hash, ele.timestamp)
 
 test_block = Chain()
 
@@ -40,7 +41,7 @@ for i in range(1, 5):
 
 test_block.display()
 
-print(test_block.size_of_chain())
+print(f'size of Blockchain - {test_block.size_of_chain()}')
 
 # print(genesis_Block.prev_hash)
 # print(block_1.prev_hash)
@@ -59,3 +60,4 @@ print(test_block.size_of_chain())
 
 # c = sha256(a.encode()).hexdigest() + sha256(b.encode()).hexdigest()
 # # print(sha256(c.encode()).hexdigest())
+
