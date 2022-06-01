c4d21ecbd253bfd53e37f3d3115eea9ea6a8ed6c
Jinarma <jinarma@gmail.com>
2022-06-01 14:35:25 +0530

    inital commit

---
+++ CPUScheduling/firstComeFirstServe.py
@@ -0,0 +1,21 @@
+def FCFS(process_list: list) -> list:
+	process_list = sorted(process_list)		# [at, bt]
+	processed_list = []		# [ct, tat, wt]
+	buff_point = process_list[0][0]
+	for i, ele in enumerate(process_list):
+		processed_list.append([buff_point+process_list[i][1]])
+		buff_point += process_list[i][1]
+		processed_list[i].append(processed_list[i][0]-process_list[i][0])
+		processed_list[i].append(processed_list[i][1]-process_list[i][1])
+	return processed_list
+
+# Driver
+process_num = int(input('Enter the number of processes: '))
+process_list = []
+for i in range(process_num):
+	process_list.append(list(map(int, input(f'Enter Arrival time and Burst time for process {i+1}: ').split())))
+processed_list = FCFS(process_list)
+
+print('Process\tArrival\tBurst\tCompletion\tTAT\tWT')
+for i, ele in enumerate(processed_list):
+	print(f'P{i}\t{process_list[i][0]}\t{process_list[i][1]}\t{processed_list[i][0]}\t\t{processed_list[i][1]}\t{processed_list[i][2]}')
