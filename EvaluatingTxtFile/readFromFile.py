eeb3f284c0fafc5946bbfe52ff1c9015e14dcd80
Jinarma <jinarma@gmail.com>
2022-06-11 19:11:15 +0530

    initial commit

---
+++ EvaluatingTxtFile/readFromFile.py
@@ -0,0 +1,3 @@
+file = open('D:\Programming\Python\Github\Python_General\EvaluatingTxtFile\\functionFile.txt', 'r')
+exec(file.read())
+file.close()
---
+++ EvaluatingTxtFile/writeToFile.py
@@ -0,0 +1,3 @@
+file = open('D:/Programming/Python/Github/Python_General/EvaluatingTxtFile/functionFile.txt', 'w')
+file.write('print("hello")\nprint("I am dumb")')
+file.close()
