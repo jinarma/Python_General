a64cf45df7d369ae6825d8c41123c8cefb32a368
Jinarma <jinarma@gmail.com>
2022-06-12 17:38:27 +0530

    resolved if AT > 0

--- CPUScheduling/ShortestRemainingTimeFirst.py
+++ CPUScheduling/ShortestRemainingTimeFirst.py
@@ -11,27 +11,35 @@ def SRTF(process_list):
 	remaining_time_total = 0
 	for _, _, rt in process_list:
 		remaining_time_total += rt
-
 	process_list.sort(key=lambda x: x[1])	# sorts based on AT
+	remaining_time_total += process_list[0][1]
 	
 	ready_queue = []
 	current_process = []
-
-	for i in range(remaining_time_total):
+	i = 0
+	# for i in range(remaining_time_total):
+	while remaining_time_total > 1:
+		remaining_time_total = 0
 		for ele in process_list:
 			if ele[1] <= i:
 				ready_queue.append(ele)
 				process_list.remove(ele)
 
 		ready_queue.sort(key=sort_positive)		# sorts the ready queue based on RT
-		current_process.append(ready_queue.pop(0))
-		print(i, current_process)
-		current_process[0][2] -= 1
-		if current_process[0][2] <= 0:
-			current_process.pop()
-			continue
-		else:
-			ready_queue.append(current_process.pop(0))
+		remaining_time_total += ele[2]
+
+		try:
+			current_process.append(ready_queue.pop(0))
+			print(i, current_process)
+			current_process[0][2] -= 1
+			if current_process[0][2] <= 0:
+				current_process.pop()
+			else:
+				ready_queue.append(current_process.pop(0))
+		except:
+			print(i, '[empty]')
+
+		i += 1
 
 
 
@@ -49,7 +57,7 @@ def SRTF(process_list):
 
 # driver code
 if __name__ == '__main__':
-	process_list = [[1, 3, 8], [2, 2, 3], [3, 6, 9], [4, 1, 4], [5, 5, 2]]		# [pID, at, bt]
+	process_list = [[1, 26, 8], [2, 2, 3], [3, 6, 9], [4, 1, 4], [5, 5, 2]]		# [pID, at, bt]
 	process_list_copy = copy.copy(process_list)
 	SRTF(process_list_copy)
 	pass
