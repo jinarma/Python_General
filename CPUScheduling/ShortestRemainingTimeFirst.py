77eb715d7d8ed1accc113eaa545f30fbb40d2e1b
Jinarma <jinarma@gmail.com>
2022-06-12 16:41:33 +0530

    unresolved if min AT > 0

--- CPUScheduling/ShortestRemainingTimeFirst.py
+++ CPUScheduling/ShortestRemainingTimeFirst.py
@@ -1,27 +1,55 @@
 import copy
 
 
+def sort_positive(num_list):
+	if num_list[2] >= 0:
+		return num_list[2]
+	else:
+		return []
+
 def SRTF(process_list):
 	remaining_time_total = 0
-	for _, _ , rt in process_list:
+	for _, _, rt in process_list:
 		remaining_time_total += rt
-	print(remaining_time_total)
 
+	process_list.sort(key=lambda x: x[1])	# sorts based on AT
+	
 	ready_queue = []
-	process_list.sort(key= lambda x: x[1])
-	# for i in range(remaining_time_total, -1, -1):
-	i = 0
-	while i < process_list[-1][0]:
-		if process_list[i][2] > 0:
-			process_list[i][2] -= 1
-			print(f'-> P{i}', end=' ')
+	current_process = []
+
+	for i in range(remaining_time_total):
+		for ele in process_list:
+			if ele[1] <= i:
+				ready_queue.append(ele)
+				process_list.remove(ele)
+
+		ready_queue.sort(key=sort_positive)		# sorts the ready queue based on RT
+		current_process.append(ready_queue.pop(0))
+		print(i, current_process)
+		current_process[0][2] -= 1
+		if current_process[0][2] <= 0:
+			current_process.pop()
+			continue
 		else:
-			i += 1
-		# print(i)
+			ready_queue.append(current_process.pop(0))
+
+
+
+
+	# process_list.sort(key= lambda x: x[1])		# sorts by RT
+	# # for i in range(remaining_time_total, -1, -1):
+	# i = 0
+	# while i < process_list[-1][0]:
+	# 	if process_list[i][2] > 0:
+	# 		process_list[i][2] -= 1
+	# 		print(f'-> P{i}', end=' ')
+	# 	else:
+	# 		i += 1
+	# 	# print(i)
 
 # driver code
 if __name__ == '__main__':
-	process_list = [[0, 3, 8], [1, 2, 3], [2, 0, 9], [3, 1, 4], [4, 5, 2]]		# [pID, at, bt]
+	process_list = [[1, 3, 8], [2, 2, 3], [3, 6, 9], [4, 1, 4], [5, 5, 2]]		# [pID, at, bt]
 	process_list_copy = copy.copy(process_list)
 	SRTF(process_list_copy)
 	pass
