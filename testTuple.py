1da0e7b37545da7b0df94f8a1b28825af0c78dba
Jinarma <jinarma@gmail.com>
2022-06-14 19:53:58 +0530

    multiple attribute sorting key

--- testTuple.py
+++ testTuple.py
@@ -1,4 +1,7 @@
-tup = (1, 2, 3, 4, 'hello')
-# ls = list(map(int, tup))
-ls = list(tup)
-print(tup, ls, sep='\n')
+def caseless_sort(tup: tuple) -> tuple:
+	return (tup[0].lower(), tup[1])		# returns (x, y) i.e if x are same, then comparison goes to y
+
+
+tuple_list = [('a', 2), ('B', 6), ('A', 1), ('c', 5)]
+print(tuple_list)
+print(sorted(tuple_list, key=caseless_sort))
