d1c68b82767730e80c1df40e0453c14049b20e6b
Jinarma <jinarma@gmail.com>
2022-06-14 19:57:50 +0530

    second attribute as descending IMPORTANT

--- testTuple.py
+++ testTuple.py
@@ -1,5 +1,6 @@
 def caseless_sort(tup: tuple) -> tuple:
-	return (tup[0].lower(), tup[1])		# returns (x, y) i.e if x are same, then comparison goes to y
+	return (tup[0].lower(), -tup[1])		# returns (x, y) i.e if x are same, then comparison goes to y
+	# adding the negative at y, we instruct it to sort in reverse i.e. descending by deault
 
 
 tuple_list = [('a', 2), ('B', 6), ('A', 1), ('c', 5)]
