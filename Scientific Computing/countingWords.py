a22ee67fed88a72b35fd1783c7ee0d541c1e4fcb
Jinarma <jinarma@gmail.com>
2022-06-14 19:54:19 +0530

    sorting based on values

--- Scientific Computing/countingWords.py
+++ Scientific Computing/countingWords.py
@@ -1,7 +1,12 @@
+def index_to_sort(ls):
+	return ls[1]
+
+
 counts = {}
 text = input().lower()
 words = text.strip(', . ; ?')
 words = words.split()
 for word in words:
 	counts[word] = counts.get(word, 0) + 1
-print(counts)
+# print(sorted(counts.items(), reverse=True))
+print(sorted(counts.items(), key=index_to_sort, reverse=True))
