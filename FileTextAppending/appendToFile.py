1abf48967d7dd48d0d408bea59b81fd2bba612c0
Jinarma <jinarma@gmail.com>
2022-06-21 22:44:58 +0530

    some messing about

--- FileTextAppending/appendToFile.py
+++ FileTextAppending/appendToFile.py
@@ -2,14 +2,14 @@ from ctypes import sizeof
 from io import SEEK_END
 import os
 
-def write(string):
+def add_to_file(string):
 	file = open('D:/Programming/Python/Github/Python_General/FileTextAppending/testString.txt', 'a')
+	file.seek(2)
 	file.write(string)
 	file.write('\n')
-	file.seek(0, SEEK_END)
 	print(file.tell(), 'bytes')
 	file.close()
 	# os.remove(file.name)	# deletes the file
 
-write('Sup brah')
+add_to_file('Sup brah')
 
