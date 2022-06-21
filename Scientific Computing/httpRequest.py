c83785f58fe259b4111034e0c8295b0f224e2fb2
Jinarma <jinarma@gmail.com>
2022-06-21 23:02:09 +0530

    fixed the error, turns out it was an issue with line endings

--- Scientific Computing/httpRequest.py
+++ Scientific Computing/httpRequest.py
@@ -1,14 +1,21 @@
 import socket
 
 
-mysock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-mysock.connect(('data.pr4e.org', 80))
-cmd = 'GET http://data.pr4e.org/romeo.txt HTTP/1.0\n\n'.encode()
-mysock.send(cmd)
+fhand = open('D:\Programming\Python\Github\Python_General\Scientific Computing\dataRecievedFromSocketCall.html', 'w')
 
-while True:
-	data = mysock.recv(8)
-	if len(data) < 1:
-		break
-	print(data.decode(), end='')
-mysock.close()
+mysock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+try:
+	mysock.connect(('data.pr4e.org', 80))
+	cmd = 'GET http://data.pr4e.org/romeo.txt HTTP/1.0\r\n\r\n'.encode()
+	mysock.send(cmd)
+
+	while True:
+		data = mysock.recv(8)
+		if len(data) < 1:
+			break
+		fhand.write(data.decode())
+		print(data.decode(), end='')
+	mysock.close()
+except Exception as error:
+	print(error)
+fhand.close()
