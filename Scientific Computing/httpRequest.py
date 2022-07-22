cbfd065c27d9c07d4b27176890e51879239a5220
Jinarma <jinarma@gmail.com>
2022-07-22 09:17:29 +0530

    a bit more testable

--- Scientific Computing/httpRequest.py
+++ Scientific Computing/httpRequest.py
@@ -1,18 +1,27 @@
 import socket
+import urllib.request
 
 
+def socket_request(url, port, page):
+	mysock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+	try:
+		mysock.connect((url, port))
+		cmd = f'GET http://{url}/{page} HTTP/1.0\r\n\r\n'.encode()
+		mysock.send(cmd)
 
-mysock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-try:
-	mysock.connect(('127.0.0.1', 8000))
-	cmd = 'GET http://127.0.0.1/playground/hello/ HTTP/1.0\r\n\r\n'.encode()
-	mysock.send(cmd)
+		while True:
+			data = mysock.recv(8)
+			if len(data) < 1:
+				break
+			print(data.decode(), end='')
+		mysock.close()
+	except Exception as error:
+		print(error)
 
-	while True:
-		data = mysock.recv(8)
-		if len(data) < 1:
-			break
-		print(data.decode(), end='')
-	mysock.close()
-except Exception as error:
-	print(error)
+def urllib_request(url):
+	fhand = urllib.request.urlopen(url)
+	for line in fhand:
+		print(line.decode().strip())
+
+socket_request('data.pr4e.org', 80, 'romeo.txt')
+# urllib_request('http://data.pr4e.org/romeo.txt')
