647a1f0b8f0404019280f20e20899fe147885422
Jinarma <jinarma@gmail.com>
2022-08-16 14:06:06 +0530

    works to permute through all numbers, not a specific limit

--- General/permutation.py
+++ General/permutation.py
@@ -1,21 +1,19 @@
 # distribute into 3's, at least one of them must be the largest
 
-def check(num):
-	temp = 0
-	for i in range(110+(num-2), (num-2)*100+12):
-		ele = str(i)
-		if '0' in ele:
-			continue
-		if sum(map(int, ele)) == num:
-			if ele[0] == ele[1] and ele[2] <= ele[0]:
-				continue
-			if ele[0] == ele[2] and ele[1] <= ele[0]:
-				continue
-			if ele[1] == ele[2] and ele[0] <= ele[1]:
-				continue
-			print(ele)
-			temp += 1
-	return temp
+def check(perm: list):
+	if len(perm) == 0:
+		return []
+	elif len(perm) == 1:
+		return [perm]
+	num_list = []
+	for i in range(len(perm)):
+		m = perm[i]
+		rem_list = perm[:i]+perm[i+1:]
+		for p in check(rem_list):
+			num_list.append([m]+p)
+	return num_list
 
-num = check(int(input()))
-print(num)
+num_list = check([1, 2, 3])
+lem = [1, 2, 3]
+print(lem[:0]+lem[1:])
+print(num_list)
