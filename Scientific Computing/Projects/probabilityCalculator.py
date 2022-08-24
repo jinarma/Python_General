de6f9be5ae1b31fe68d908b1abcf2b3f085ec1d2
Jinarma <jinarma@gmail.com>
2022-08-24 13:13:02 +0530

    changed draw

--- Scientific Computing/Projects/probabilityCalculator.py
+++ Scientific Computing/Projects/probabilityCalculator.py
@@ -5,26 +5,28 @@ import random
 
 class Hat:
 	
-	def __init__(self, *args, **kwargs):
+	def __init__(self, **kwargs):
 		self.contents = []
-		print(kwargs)
 		for key, value in kwargs.items():
 			self.contents += ([key]*value)
-		# print(self.contents)
-		
+
 	def draw(self, removed):
 		if removed > len(self.contents):
 			return self.contents
-		else:
-			res = 0
-			return res
+		res = []
+		for i in range(removed):
+			res.append(self.contents.pop(random.randint(0, len(self.contents))))
+		# print(self.contents)
+		# print(res)
+		return res
 
 
 
 def experiment(hat, expected_balls, num_balls_drawn, num_experiments):
 	m, n = 0, num_experiments
 	for _ in range(num_experiments):
-		hat_copy = copy.copy(hat)
+		hat_copy = copy.deepcopy(hat)
+		# print(id(hat), id(hat_copy))
 		# print(hat_copy.draw(3))
 		all_balls = 0
 		hat_removed_dict = {}
@@ -45,7 +47,8 @@ def experiment(hat, expected_balls, num_balls_drawn, num_experiments):
 	# print(m)
 	return m/n
 
-# random.seed(95)
+random.seed(95)
 hat = Hat(blue=3, red=2, green=6)
-# print(hat.draw(3))
+hat.draw(3)
 # print(experiment(hat, {'blue':2, 'green':1}, 4, 1000))
+print(hat)
